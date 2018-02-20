58d6a227f0c932c23ffb6f855e093b2e4e114bab
vigneshakv <36235477+vigneshakv@users.noreply.github.com>
2018-02-20 15:59:57 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter an number : \n");
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("number : %d is even\n",a);
+    else
+    printf("number : %d is odd\n",a);
+}
+
+
+
+
+
